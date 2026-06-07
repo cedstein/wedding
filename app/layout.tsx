fd4360b45f2257aa6d0ed4e32d950d9d68827335
Cecilia Edstein <cecilia.edstein@gmail.com>
2026-06-07 20:21:12 +0200

    Implement Person model with Mongoose and UUID for unique IDs; update layout and navigation structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,14 +29,13 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <nav>
-          <Link href={"Start"}>
-            <Link href={"Info"}></Link>
-          </Link>
-          <Link href={"OSA"}></Link>
+        <nav className="flex justify-end p-2 ">
+          <Link href={"Start"}>Start</Link>
+          <Link href={"Info"}>Info</Link>
+          <Link href={"OSA"}>OSA</Link>
         </nav>
         <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-          <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+          <main className="flex flex-1 w-full  flex-col items-center py-32 px-16 bg-white dark:bg-black sm:items-start">
             {children}
           </main>
         </div>
---
+++ app/models/Person.ts
@@ -0,0 +1,22 @@
+import { model, models, Schema } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
+const personSchema = new Schema({
+  id: { type: String, required: true, default: () => uuidv4() },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  email: { type: String, required: true },
+  attending: { type: Boolean, required: false },
+  attendingChurch: { type: Boolean, required: false },
+  attendingParty: { type: Boolean, required: false },
+  // Låtar
+  song1Artist: { type: String, required: false },
+  song1Title: { type: String, required: false },
+  song2Artist: { type: String, required: false },
+  song2Title: { type: String, required: false },
+  // Allergi
+  hasAllergies: { type: Boolean, required: false },
+  allergyText: { type: String, required: false },
+});
+
+export const Person = models.person || model("person", personSchema);
